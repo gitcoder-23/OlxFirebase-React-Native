b84ffb04a68d701bb52cd2532efb65f84549f563
prabhat-cn <prabhat@capitalnumbers.com>
2022-09-02 20:12:16 +0530

    not fully completed..but most part covered

--- src/screens/CreateAd/CreateAdScreen.js
+++ src/screens/CreateAd/CreateAdScreen.js
@@ -11,6 +11,7 @@ import firestore from '@react-native-firebase/firestore';
 import auth from '@react-native-firebase/auth';
 import storage from '@react-native-firebase/storage';
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import ImageCropPicker from 'react-native-image-crop-picker';
 
 const CreateAdScreen = () => {
   const [name, setName] = useState('');
@@ -18,12 +19,15 @@ const CreateAdScreen = () => {
   const [year, setYear] = useState('');
   const [price, setPrice] = useState('');
   const [phone, setPhone] = useState('');
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState(null);
+  const [uploading, setUploading] = useState(false);
+  const [transfered, setTransfered] = useState(0);
 
   const postData = async () => {
-    if (!name || !desc || !year || !price || !phone) {
+    if (!name || !desc || !year || !price || !phone || !image) {
       Alert.alert('', 'Please fill all the fields');
     } else {
+      submitPhoto();
       const usersCollection = firestore().collection('ads');
       try {
         let result = await usersCollection.add({
@@ -32,8 +36,7 @@ const CreateAdScreen = () => {
           year,
           price,
           phone,
-          image:
-            'https://images.unsplash.com/photo-1640437830863-8f7f38327319?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80',
+          image,
           uid: auth().currentUser.uid,
         });
         Alert.alert('', 'Ad posted!', [
@@ -54,12 +57,107 @@ const CreateAdScreen = () => {
         Alert.alert('', 'Something went wrong. Try again');
       }
     }
+    // submitPhoto();
   };
 
-  const openCamera = () => {
-    launchCamera({quality: 0.5}, fileObj => {
-      console.log('fileObj->', fileObj);
+  // const openCamera = () => {
+  //   launchCamera({quality: 0.5}, fileObj => {
+  //     console.log('fileObjA->', fileObj.assets[0].uri, typeof fileObj);
+  //     const uploadTask = storage()
+  //       .ref()
+  //       .child(`/items/${Date.now()}`)
+  //       // .putFile(fileObj && fileObj.assets ? fileObj.assets[0].uri : '');
+  //       .putFile(fileObj.uri);
+
+  //     // operation
+  //     uploadTask.on(
+  //       'state_changed',
+  //       snapshot => {
+  //         var progress =
+  //           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+  //         console.log('Upload is ' + progress + '% done');
+
+  //         if (progress == 100) {
+  //           Alert.alert('', 'Uploaded!');
+  //         }
+  //       },
+  //       error => {
+  //         // Handle unsuccessful uploads
+  //         Alert.alert('', 'Something went wrong!');
+  //       },
+  //       () => {
+  //         // Handle successful uploads on complete
+  //         // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+  //         uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
+  //           console.log('File available at', downloadURL);
+  //           setImage(downloadURL);
+  //         });
+  //       },
+  //     );
+  //   });
+  // };
+
+  // const openCamera = () => {
+  //   launchImageLibrary({quality: 0.5}, fileobj => {
+  //     console.log(fileobj);
+  //     const uploadTask = storage()
+  //       .ref()
+  //       .child(`/items/${Date.now()}`)
+  //       .putFile(fileobj && fileobj.assets ? fileobj.assets[0].uri : '');
+  //     uploadTask.on(
+  //       'state_changed',
+  //       snapshot => {
+  //         var progress =
+  //           (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+  //         if (progress == 100) {
+  //           alert('uploaded');
+  //         }
+  //       },
+  //       error => {
+  //         alert('something went wrong');
+  //       },
+  //       () => {
+  //         // Handle successful uploads on complete
+  //         // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+  //         uploadTask.snapshot.ref.getDownloadURL().then(downloadURL => {
+  //           setImage(downloadURL);
+  //         });
+  //       },
+  //     );
+  //   });
+  // };
+
+  const takePhotoFromCamera = async () => {
+    ImageCropPicker.openCamera({
+      width: 300,
+      height: 400,
+      cropping: true,
+    }).then(image => {
+      // console.warn(image.path);
+      setImage(image.path);
+      this.bs.current.snapTo(1);
     });
+  };
+
+  console.log('image->', image);
+
+  const submitPhoto = async () => {
+    const uploadUri = image;
+    let fileName = await uploadUri.substring(uploadUri.lastIndexOf('/') + 1);
+    console.log('fileName->', fileName);
+    setUploading(true);
+    try {
+      await storage()
+        .ref(fileName)
+        .child(`/items/${Date.now()}`)
+        .putFile(uploadUri);
+      setUploading(false);
+      Alert.alert('', 'Image uploaded');
+    } catch (error) {
+      console.warn('error->', error);
+      // Alert.alert('', 'Something wrong!!');
+    }
+    setImage(null);
   };
 
   return (
@@ -103,7 +201,10 @@ const CreateAdScreen = () => {
           value={phone}
           onChangeText={text => setPhone(text)}
         />
-        <Button icon="camera" mode="contained" onPress={() => openCamera()}>
+        <Button
+          icon="camera"
+          mode="contained"
+          onPress={() => takePhotoFromCamera()}>
           Upload Image
         </Button>
         <Button mode="contained" onPress={() => postData()}>
